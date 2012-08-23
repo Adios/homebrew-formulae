1491af9ac2f021e1d96849bb05b0d1d6a3a435b9
Adios <adiosf6f+github@gmail.com>
2012-08-23 21:32:51 +0800

    gcc: update to 4.7.1.

--- gcc.rb
+++ gcc.rb
@@ -2,20 +2,23 @@ require 'formula'
 
 class Gcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.4.7/gcc-4.4.7.tar.bz2'
-  md5 '295709feb4441b04e87dea3f1bab4281'
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.7.1/gcc-4.7.1.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.7.1/gcc-4.7.1.tar.bz2'
+  sha1 '3ab74e63a8f2120b4f2c5557f5ffec6907337137'
 
   depends_on 'gmp'
+  depends_on 'libmpc'
   depends_on 'mpfr'
 
   skip_clean :all
 
   def install
-    gmp = Formula.factory 'gmp'
-    mpfr = Formula.factory 'mpfr'
-
     # GCC will suffer build errors if forced to use a particular linker.
     ENV.delete 'LD'
+
+    gmp = Formula.factory 'gmp'
+    mpfr = Formula.factory 'mpfr'
+    libmpc = Formula.factory 'libmpc'
 
     # Sandbox the GCC lib, libexec and include directories so they don't wander
     # around telling small children there is no Santa Claus. This results in a
@@ -25,20 +28,38 @@ class Gcc < Formula
     gcc_prefix = prefix + 'gcc'
 
     args = [
+      # Sandbox everything...
       "--prefix=#{gcc_prefix}",
-      "--datadir=#{share}",
+      # ...except the stuff in share...
+      "--datarootdir=#{share}",
+      # ...and the binaries...
       "--bindir=#{bin}",
-      "--program-suffix=-#{version.slice(/\d\.\d/)}",
+      # ...which are tagged with a suffix to distinguish them.
+      "--program-suffix=-#{version.to_s.slice(/\d\.\d/)}",
       "--with-gmp=#{gmp.prefix}",
       "--with-mpfr=#{mpfr.prefix}",
+      "--with-mpc=#{libmpc.prefix}",
       "--with-system-zlib",
-      "--enable-checking"
+      "--enable-checking",
+      "--enable-plugin",
+      "--enable-lto",
+      "--disable-multilib"
     ]
 
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
+    mkdir 'build' do
+      unless MacOS::CLT.installed?
+        # For Xcode-only systems, we need to tell the sysroot path.
+        # 'native-system-header's will be appended
+        args << "--with-native-system-header-dir=/usr/include"
+        args << "--with-sysroot=#{MacOS.sdk_path}"
+      end
+
       system '../configure', "--enable-languages=c,c++", *args
       system 'make bootstrap'
+
+      # At this point `make check` could be invoked to run the testsuite. The
+      # deja-gnu and autogen formulae must be installed in order to do this.
+
       system 'make install'
     end
   end
